c0831890509ffcd91ad431d727f7cec5f4dfe772
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-17 14:06:38 -0500

    added to header

--- sort.h
+++ sort.h
@@ -1,9 +1,19 @@
 #ifndef SORTING_ALG
 #define SORTING_ALG
 
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Prototypes for mandatory tasks */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+size_t partition(int array[], size_t size);
+void print_sort(int array[], size_t size, int init);
 
 #endif
