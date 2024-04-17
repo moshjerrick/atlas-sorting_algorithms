f0101168802801ed2b77b91e1f1b1e662710d52b
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-17 14:03:48 -0500

    bubble load

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,14 +6,22 @@
  * 
  * 
 */
-void bubble_sort(int *array, size_t size)
-{
 
+void bubble_sort(int *array, size_t size) {
+    for (size_t i = 0; i < size - 1; i++) {
+        for (size_t j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
 
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
 
-
-
-
-
-    
+                printf("Array after swapping: ");
+                for (size_t k = 0; k < size; k++) {
+                    printf("%d ", array[k]);
+                }
+                printf("\n");
+            }
+        }
+    }
 }
